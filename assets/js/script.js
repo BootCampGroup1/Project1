20ae76d0b84acdda9ee1453ebbecc016d38d7383
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-05-09 20:54:10 -0500

    Co-authored-by: rylawss <rylawss@users.noreply.github.com> Co-authored-by: teylorsmith34 <teylorsmith34@users.noreply.github.com> Co-authored-by: jdiego-17 <jdiego-17@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,34 @@ $("#btn").on("click", function (event) {
     url: "https://api.api-ninjas.com/v1/nutrition?query=" + query,
     headers: { "X-Api-Key": "HRzJF2BzvHXQKhYPJEVAUA==wkK322BmJcvvLHPt" },
     contentType: "application/json",
-    success: successFunction,
+    success: foodFunction,
     error: function ajaxError(jqXHR) {
       console.error("Error: ", jqXHR.responseText);
     },
   });
 });
-//
-var calorycalc;
-function successFunction(result) {
+// Displays nutrition API results in ul
+// var calorycalc;
+function foodFunction(result) {
   console.log(result);
   for (var i = 0; i < result.length; i++) {
-    console.log(result[i].name, result[i].calories);
-    $("#results").append(
+    var food = result[i].name;
+    var calories = result[i].calories;
+    var servingSize = result[i].serving_size_g;
+    console.log(food, calories);
+    $("#foodresults").append(
       `<li>
-        Name: ${result[i].name} Calories: <span>${Math.round(result[i].calories)}</span>
+        Name: ${food} Calories: <span class="caldata">${Math.round(calories)}</span>
         <div class="slidecontainer">
-        <input data-food="${result[i].name}" data-cals="${result[i].calories}" type="range" min="0" max="500" step="10" value="${result[i].serving_size_g}" class="slider"><p>100</p>
+        <input data-food="${food}" data-cals="${calories}" type="range" min="0" max="500" step="10" value="${servingSize}" class="slider"><p>100</p>
         </div>
         </li>
     `
     );
   }
+  totalCalories();
 }
-$("#results").on("input", '[type="range"]', function (event) {
+$("#foodresults").on("input", '[type="range"]', function (event) {
   var slider = $(event.target);
   console.log(slider.val());
   var serving = slider.val();
@@ -39,4 +43,48 @@ $("#results").on("input", '[type="range"]', function (event) {
   console.log(calorycalc);
   slider.parent().parent().find("span").text(Math.round(calorycalc));
   slider.parent().parent().find("p").text(serving);
+  totalCalories();
 });
+
+var totalCalories = function() {
+  var sumCals = 0
+  for (var i=0; i < $('.caldata').length; i++) {
+    var spanVal = $('.caldata')[i];
+    var foodCals = parseInt(spanVal.innerHTML);
+    sumCals += foodCals
+  }
+  console.log(sumCals);
+};
+
+//random button
+//Second API call
+var activity = "ball";
+$.ajax({
+  method: "GET",
+  url: "https://api.api-ninjas.com/v1/caloriesburned?activity=" + activity,
+  headers: { "X-Api-Key": "HRzJF2BzvHXQKhYPJEVAUA==wkK322BmJcvvLHPt" },
+  contentType: "application/json",
+  success: sportFunction,
+  error: function ajaxError(jqXHR) {
+    console.error("Error: ", jqXHR.responseText);
+  },
+});
+
+// Displays activity API results in ul
+
+function sportFunction(result) {
+  console.log(result);
+  for (var i = 0; i < result.length; i++) {
+    var sport = result[i].name;
+    var calBurned = result[i].calories_per_hour;
+    var time = result[i].duration_minutes;
+    console.log(sport, calBurned);
+    $("#sportresults").append(
+      `<li>
+        Name: ${sport} Calories: <span>${calBurned}</span> Time: ${time}
+        </li>
+    `
+    );
+  }
+}
+
